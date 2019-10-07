fd1ff7d99fc3d2fe2495cf6766b3bd2c5897b6f8
chrisvltn <chris_valentin@outlook.com>
2019-10-07 16:48:49 +0200

    Adds personal email

--- tasks/keyword-expander.lua
+++ tasks/keyword-expander.lua
@@ -3,7 +3,8 @@ ht = hs.loadSpoon("HammerText")
 ht.keywords ={
     ["[["] = "$",
     ["@date"] = function() return os.date("%Y-%m-%d") end,
-    ["@email"] = "chris.valentin@westwing.de",
+    ["@email"] = "chris_valentin@outlook.com",
+    ["@wemail"] = "chris.valentin@westwing.de",
     ["@emoji"] = function() hs.eventtap.keyStroke({"cmd", "ctrl"}, "space") end,
 }
 ht:start()
