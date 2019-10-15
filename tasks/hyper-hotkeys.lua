4eb355cfda61380f8a72cd0863945f396e5e7c2f
Christian Valentin <christian.valentin@westwing.de>
2019-10-15 15:44:56 +0200

    Adds possibililty to have two apps in one hyper hotkey

--- tasks/hyper-hotkeys.lua
+++ tasks/hyper-hotkeys.lua
@@ -7,7 +7,7 @@ local hyper = hs.hotkey.modal.new()
 local appHotkeys = {
   { "c", 'Google Chrome' },
   { "t", 'iTerm' },
-  { "s", 'Slack' },
+  { "s", {'Slack', 'Spotify'} },
   { "v", 'Visual Studio Code' },
   { "d", 'Microsoft Outlook' },
   { "e", 'Finder' },
@@ -15,6 +15,7 @@ local appHotkeys = {
 }
 
 local logger = hs.logger.new('AppHotkeys', 'debug')
+logger.d('Working')
 
 for i, hotkey in ipairs(appHotkeys) do
     local key = hotkey[1]
@@ -22,6 +23,35 @@ for i, hotkey in ipairs(appHotkeys) do
 
     if type(applicationName) == "function" then
         hyper:bind({}, key, applicationName)
+    elseif type(applicationName) == "table" then
+        local appNameIndexes = {}
+        for k, v in pairs(applicationName) do
+            appNameIndexes[v] = k
+        end
+        
+        hyper:bind({}, key, function()
+            local apps = hs.application.runningApplications()
+            local frontMostApp
+
+            for key, app in pairs(apps) do
+                if app.isFrontmost(app) then
+                    frontMostApp = app
+                end
+            end
+
+            if frontMostApp ~= nil then
+                local title = frontMostApp.title(frontMostApp)
+                local index = appNameIndexes[title]
+
+                if index == nil or applicationName[index + 1] == nil then
+                    logger.df("Opening %s", applicationName[1])
+                    hs.application.open(applicationName[1])
+                else
+                    logger.df("Opening %s", applicationName[index + 1])
+                    hs.application.open(applicationName[index + 1])
+                end
+            end
+        end)
     else
         hyper:bind({}, key, function()
             hs.application.open(applicationName)
