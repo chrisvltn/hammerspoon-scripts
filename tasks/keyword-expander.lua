d6a593b4717521cc1e0254075953fc6df55ffecb
chrisvltn <chris_valentin@outlook.com>
2019-10-24 17:06:50 +0200

    Fix work email key expander

--- tasks/keyword-expander.lua
+++ tasks/keyword-expander.lua
@@ -4,7 +4,7 @@ ht.keywords ={
     ["[["] = "$",
     ["@date"] = function() return os.date("%Y-%m-%d") end,
     ["@email"] = "chris_valentin@outlook.com",
-    ["@wemail"] = "chris.valentin@westwing.de",
+    ["@wemail"] = "christian.valentin@westwing.de",
     ["@emoji"] = function() hs.eventtap.keyStroke({"cmd", "ctrl"}, "space") end,
 }
 ht:start()
