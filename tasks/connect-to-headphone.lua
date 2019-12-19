f7054448763f739546b8ab08f900a06c1438de23
chrisvltn <chris_valentin@outlook.com>
2019-12-19 16:13:25 +0100

    Change bluetil with ootb bluetooth device listing

--- tasks/connect-to-headphone.lua
+++ tasks/connect-to-headphone.lua
@@ -1,10 +1,10 @@
 local logger = hs.logger.new('Connect to headphones', 'debug')
 
 -- Check and install dependencies if needed
-local output, isOK = hs.execute('blueutil -v', true)
+local output, isOK = hs.execute('blueutil -v')
 
 if not isOK then
-    hs.execute('brew install blueutil', true)
+    hs.execute('brew install blueutil')
 end
 
 local BLUETOOTH_DEVICE_NAME = "Bozo"
@@ -12,23 +12,33 @@ local BLUETOOTH_DEVICE_NAME = "Bozo"
 -- Cmd + Alt + Ctrl + B to reload connect to the headphone
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "B", function()
     hs.alert.show("Connecting to " .. BLUETOOTH_DEVICE_NAME .. "...")
-    local output, isOK = hs.execute('blueutil --recent --format json-pretty', true)
-    local devices = hs.json.decode(output)
-
-    local selectedDevice = hs.fnutils.find(devices, function (device) 
-        return device.name == BLUETOOTH_DEVICE_NAME
-    end)
-
-    if selectedDevice.connected then
-        hs.alert.show(BLUETOOTH_DEVICE_NAME .. " already connected")
-    end
+    local output, isOK = hs.execute('system_profiler -json SPBluetoothDataType')
+    local devices = hs.json.decode(output).SPBluetoothDataType[1].device_title
+    local selectedDevice = nil
     
-    if selectedDevice ~= nil and not selectedDevice.connected then
-        hs.execute('blueutil --connect ' .. selectedDevice.address, true)
-        hs.alert.show(BLUETOOTH_DEVICE_NAME .. " connected")
+    for index, device in pairs(devices) do
+        for key in pairs(device) do
+            if key == BLUETOOTH_DEVICE_NAME then
+                selectedDevice = device[key]
+                break
+            end
+        end
+
+        if selectedDevice ~= nil then
+            break
+        end
     end
 
     if selectedDevice == nil then
         hs.alert.show(BLUETOOTH_DEVICE_NAME .. " not found")
+        return
+    end
+
+    local isConnected = selectedDevice.device_isconnected == "attrib_Yes"
+    if isConnected then
+        hs.alert.show(BLUETOOTH_DEVICE_NAME .. " already connected")
+    else
+        hs.execute('blueutil --connect ' .. BLUETOOTH_DEVICE_NAME, true)
+        hs.alert.show(BLUETOOTH_DEVICE_NAME .. " connected")
     end
 end)
