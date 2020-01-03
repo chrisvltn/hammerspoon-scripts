b40342b727af927dfb1f02c74622e5d121d834c6
chrisvltn <chris_valentin@outlook.com>
2020-01-03 21:21:12 +0100

    Adds requently restart to middleclick task

--- tasks/reload middleclick hotkey.lua
+++ tasks/reload middleclick hotkey.lua
@@ -1,5 +1,4 @@
--- Cmd + Alt + Ctrl + M to reload MiddleClick app
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
+function restartMiddleClick()
     local app = hs.application.get('MiddleClick')
 
     if app ~= nil then
@@ -7,5 +6,17 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
     end
 
     hs.application.open('MiddleClick')
+end
+
+-- Cmd + Alt + Ctrl + M to reload MiddleClick app
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
+    restartMiddleClick()
     hs.alert.show("MiddleClick restarted!")
 end)
+
+function recurrentRestart()
+    restartMiddleClick()
+    hs.timer.doAfter(60, recurrentRestart)
+end
+
+recurrentRestart()
