6f89a2e9d40450200dadf8a3fdc538532db96315
Christian Valentin <chris_valentin@outlook.com>
2020-02-06 10:03:35 +0100

    Move HammerText to global scope to avoid garbage collection

--- Spoons/HammerText.spoon/init.lua
+++ Spoons/HammerText.spoon/init.lua
@@ -30,106 +30,114 @@
 --- 	This is intentional since the hotstring could be a part of the expanded string and this could cause a loop.
 --- 	In that case you have to type one of the "buffer-clearing" keys that are included in the "navigational keys" conditional (which is very often the case).
 
-local obj = {}
-obj.__index = obj
+HammerText = {}
+HammerText.__index = HammerText
 
 -- Metadata
-obj.name = "HammerText"
-obj.version = "1.0"
-obj.author = "Multiple Authors"
+HammerText.name = "HammerText"
+HammerText.version = "1.0"
+HammerText.author = "Multiple Authors"
 
 --- HammerText.logger
 --- Variable
 --- Logger object used within the Spoon. Can be accessed to set the default log level for the messages coming from the Spoon.
-obj.logger = hs.logger.new('HammerText', 'info')
+HammerText.logger = hs.logger.new('HammerText', 'debug')
 
 --- HammerText.keywords
 --- Variable
 --- Map of keywords to strings or functions that return a string
 --- to be replaced.
-obj.keywords = {
+HammerText.keywords = {
 	["..name"] = "My name",
 	["..addr"] = "My address",
 }
 
-function expander()
-	local word = ""
-	local keyMap = require"hs.keycodes".map
-	local keyWatcher
-	local isExpanding = false
-	local currentOutput = ""
-	local finalOutput = ""
+HammerText.watchers = {}
+HammerText.watchersIndex = 1
+HammerText.keyMap = require"hs.keycodes".map
 
+function expander()
+	HammerText.watchersIndex = HammerText.watchersIndex + 1
+
+	HammerText.watchers[HammerText.watchersIndex] = {}
+	HammerText.watchers[HammerText.watchersIndex].word = ""
+	HammerText.watchers[HammerText.watchersIndex].isExpanding = false
+	HammerText.watchers[HammerText.watchersIndex].currentOutput = ""
+	HammerText.watchers[HammerText.watchersIndex].finalOutput = ""
+	
 	-- create an "event listener" function that will run whenever the event happens
-	keyWatcher = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(ev)
+	HammerText.watchers[HammerText.watchersIndex].listener = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(ev)
 		local keyCode = ev:getKeyCode()
 		local char = ev:getCharacters()
+		local flags = ev:getFlags()
 
-		if isExpanding == true then
-			if keyCode == keyMap["delete"] then
+		if HammerText.watchers[HammerText.watchersIndex].isExpanding == true then
+			if keyCode == HammerText.keyMap["delete"] then
 				return false
 			end
 			
-			currentOutput = currentOutput .. char
-			obj.logger.d("Is expanding")
-			obj.logger.df("Current expansion '%s' (%s)", currentOutput, string.len(currentOutput))
-			obj.logger.df("Final expansion '%s' (%s)", finalOutput, string.len(finalOutput))
+			HammerText.watchers[HammerText.watchersIndex].currentOutput = HammerText.watchers[HammerText.watchersIndex].currentOutput .. char
+			HammerText.logger.d("Is expanding")
+			HammerText.logger.df("Current expansion '%s' (%s)", HammerText.watchers[HammerText.watchersIndex].currentOutput, string.len(HammerText.watchers[HammerText.watchersIndex].currentOutput))
+			HammerText.logger.df("Final expansion '%s' (%s)", HammerText.watchers[HammerText.watchersIndex].finalOutput, string.len(HammerText.watchers[HammerText.watchersIndex].finalOutput))
 			
-			if currentOutput == finalOutput then
-				obj.logger.d("Finished expansion")
-				currentOutput = ""
-				finalOutput = ""
-				isExpanding = false
+			if HammerText.watchers[HammerText.watchersIndex].currentOutput == HammerText.watchers[HammerText.watchersIndex].finalOutput then
+				HammerText.logger.d("Finished expansion")
+				HammerText.watchers[HammerText.watchersIndex].currentOutput = ""
+				HammerText.watchers[HammerText.watchersIndex].finalOutput = ""
+				HammerText.watchers[HammerText.watchersIndex].isExpanding = false
 			end
 
 			return false
 		end
 		
 		-- if "delete" key is pressed
-		if keyCode == keyMap["delete"] then
-			local flags = ev:getFlags()
-
+		if keyCode == HammerText.keyMap["delete"]
+		or keyCode == HammerText.keyMap["left"] then
 			if flags["cmd"]
 			or flags["alt"] then
-				word = ""
+				HammerText.watchers[HammerText.watchersIndex].word = ""
 				return false
 			end
 			
-			if #word > 0 then
+			if #HammerText.watchers[HammerText.watchersIndex].word > 0 then
 				-- remove the last char from a string with support to utf8 characters
 				local t = {}
-				for _, chars in utf8.codes(word) do table.insert(t, chars) end
+				for _, chars in utf8.codes(HammerText.watchers[HammerText.watchersIndex].word) do table.insert(t, chars) end
 				table.remove(t, #t)
-				word = utf8.char(table.unpack(t))
-				obj.logger.df("Word after deleting: %s", word)
+				HammerText.watchers[HammerText.watchersIndex].word = utf8.char(table.unpack(t))
+				HammerText.logger.df("Word after deleting: %s", HammerText.watchers[HammerText.watchersIndex].word)
 			end
 			return false -- pass the "delete" keystroke on to the application
 		end
 
-		-- append char to "word" buffer
-		word = word .. char
-		obj.logger.df("Word after appending: %s", word)
-
 		-- if one of these "navigational" keys is pressed
-		if keyCode == keyMap["return"]
-		or keyCode == keyMap["cmd"]
-		or keyCode == keyMap["space"]
-		or keyCode == keyMap["up"]
-		or keyCode == keyMap["down"]
-		or keyCode == keyMap["left"]
-		or keyCode == keyMap["right"] then
-			word = "" -- clear the buffer
+		if keyCode == HammerText.keyMap["return"]
+		or keyCode == HammerText.keyMap["cmd"]
+		or flags["cmd"]
+		or keyCode == HammerText.keyMap["alt"]
+		or keyCode == HammerText.keyMap["F17"]
+		or keyCode == HammerText.keyMap["tab"]
+		or keyCode == HammerText.keyMap["space"]
+		or keyCode == HammerText.keyMap["up"]
+		or keyCode == HammerText.keyMap["down"]
+		or keyCode == HammerText.keyMap["right"] then
+			HammerText.watchers[HammerText.watchersIndex].word = "" -- clear the buffer
 			return false
 		end
 
-		obj.logger.df("Word to check if hotstring: %s", word)
+		-- append char to "word" buffer
+		HammerText.watchers[HammerText.watchersIndex].word = HammerText.watchers[HammerText.watchersIndex].word .. char
+		HammerText.logger.df("Word after appending: %s", HammerText.watchers[HammerText.watchersIndex].word)
+
+		HammerText.logger.df("Word to check if hotstring: %s", HammerText.watchers[HammerText.watchersIndex].word)
 
 		-- finally, if "word" is a hotstring
-		local output = obj.keywords[word]
+		local output = HammerText.keywords[HammerText.watchers[HammerText.watchersIndex].word]
 		if type(output) == "function" then -- expand if function
 			local _, o = pcall(output)
 			if not _ then
-				obj.logger.ef("~~ expansion for '" .. what .. "' gave an error of " .. o)
+				HammerText.logger.ef("~~ expansion for '" .. what .. "' gave an error of " .. o)
 				-- could also set o to nil here so that the expansion doesn't occur below, but I think
 				-- seeing the error as the replacement will be a little more obvious that a print to the
 				-- console which I may or may not have open at the time...
@@ -138,16 +146,17 @@ function expander()
 			output = o
 		end
 
-		if output or
-		type(obj.keywords[word]) == "function" then
-			for i = 1, utf8.len(word), 1 do hs.eventtap.keyStroke({}, "delete", 0) end -- delete the abbreviation
-		end
-
 		if output then
-			isExpanding = true
-			finalOutput = output
+			-- delete the abbreviation
+			for i = 1, utf8.len(HammerText.watchers[HammerText.watchersIndex].word), 1 do 
+				hs.eventtap.keyStroke({}, "delete", 0) 
+			end 
+
+			-- starts expansion
+			HammerText.watchers[HammerText.watchersIndex].isExpanding = true
+			HammerText.watchers[HammerText.watchersIndex].finalOutput = output
 			hs.eventtap.keyStrokes(output) -- expand the word
-			word = "" -- clear the buffer
+			HammerText.watchers[HammerText.watchersIndex].word = "" -- clear the buffer
 		end
 
 		return false -- pass the event on to the application
@@ -163,9 +172,9 @@ end
 ---
 --- Parameters:
 ---  * None
-function obj:start()
+function HammerText:start()
 	print("Heeey! Hammertext is running")
 	expander()
 end
 
-return obj
+return HammerText
