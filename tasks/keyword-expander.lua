9fbc64ac9e3babf31b0279e8e20ac95eacf8e1f0
chrisvltn <chris_valentin@outlook.com>
2020-10-26 12:26:43 +0100

    adds shrug to keyword expander

--- tasks/keyword-expander.lua
+++ tasks/keyword-expander.lua
@@ -1,10 +1,10 @@
 -- Keywords expansion
 ht = hs.loadSpoon("HammerText")
-ht.keywords ={
-    ["[["] = "$",
+ht.keywords = {
     ["@date"] = function() return os.date("%Y-%m-%d") end,
     ["@email"] = "chris_valentin@outlook.com",
     ["@wemail"] = "christian.valentin@westwing.de",
     ["@emoji"] = function() hs.eventtap.keyStroke({"cmd", "ctrl"}, "space") end,
+    [".shrug."] = "¯\\_(ツ)_/¯",
 }
 ht:start()
